dc1db504926d7fbe97e15a5b33916daf61aff351
alex <allexandersokollov@gmail.com>
2023-01-14 10:43:24 +0300

    minor changes. is all good

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import article.views
 
 urlpatterns = [
-    path('', article.views.show_articles, name='index')
+    path('', article.views.show_articles, name='index'),
+    path('newArticle/', article.views.new_article, name='newArticle')
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from article.models import Article
+
 
 def show_articles(request):
-    return render(request, 'index.html')
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+
+def new_article(request):
+    return render(request, 'newArticle.html')
